12601bbc68aabf0fef0dda2111451d170471c7b6
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2024-09-21 14:32:42 +0200

    feat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
-import "../components/App.module.css";
+import '../components/App.module.css';
+import ContactForm from './ContactForm/ContactForm.jsx';
+import ContactList from './ContactList/ContactList.jsx';
+import SearchBox from './SearchBox/SeatchBox.jsx';
 
 function App() {
-  return <></>;
+	return (
+		<div>
+			<h1>Phonebook</h1>
+			<ContactForm />
+			<SearchBox />
+			<ContactList />
+		</div>
+	);
 }
 
 export default App;
